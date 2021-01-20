a67b79205e21d49e009460a4ac179303131d33f4
MuhammedAsharaf <53389411+MuhammedAshraf2020@users.noreply.github.com>
2021-01-21 00:37:01 +0200

    Update img_colorization.py

--- img_colorization.py
+++ img_colorization.py
@@ -4,22 +4,27 @@ from decodingModel import *
 # Using Transfer learning
 feature_extract_model = VggModel()
 
-PrepareData(datapath = "/content/data" , savepath = "/content/processed/"
-			target_size = (224 , 224) , batch_size = 32 , apply = feature_extract_model)
-
+#Decoding model
 colorize = model()
 
+#prepare data in hard disk
+PrepareData(datapath = "/content/data" , save_file = "/content/processed/" , 
+			target_size = (224 , 224) , batch_size = 32 , feature_extract_model = feature_extract_model)
+
+
 training_dir = "/content/processed"
-num_train_samples = 2000
-batch_size = 100
+num_train_samples = 1000
+batch_size = 32
 steps_per_epoch = np.floor(num_train_samples/batch_size)
-epochs = 501
+epochs = 200
 
 for i in range(epochs):
   generator = data_generator_baseline(training_dir, num_train_samples, batch_size)
-  fit_history = model.fit_generator(generator, epochs=1, steps_per_epoch=steps_per_epoch, verbose=1)
-  if epochs % 10 == 0:
-    model.save('model_merge_' + str(i) + '.h5')
-    X = test_images(path = "/content/oldes" , shape = (224 , 224) , batch_size = 4 ,
-                 feature_extract_model = feature_extract_model , model = model )
+  fit_history = colorize.fit_generator(generator, epochs=1, steps_per_epoch=steps_per_epoch, verbose=1)
+  if i % 10 == 0:
+    colorize.save('model_merge_' + str(i) + '.h5')
+    X = test_images(path = "/content/oldes" , shape = (224 , 224) , batch_size = 2 ,
+                 feature_extract_model = feature_extract_model , model = colorize )
+    show_images(X , width = 20 , hight = 20 , columns = 2 , rows = 1)
+
 
